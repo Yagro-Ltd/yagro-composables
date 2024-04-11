54a28c3d8344627b4ae3ed9902d80331b96081b8
Sam Wells <s.wells@yagro.com>
2024-04-11 14:39:38 +0100

    feat: Improve extract file extension function

--- src/composables/useExtractFileExtension.ts
+++ src/composables/useExtractFileExtension.ts
@@ -1,22 +1,38 @@
 /**
  * useExtractFileExtension
  * Extracts the extension from a string, excluding the filename
- * @param text - The filename with extension
+ * @param input The text string, or an object to allow for options
  * @returns The extension
- * @example useExtractFileExtension('file.txt') // 'txt'
+ * @example useExtractFileExtension({ text: 'file.txt' }) => 'txt'
+ * @example useExtractFileExtension({ text: 'file.txt', includeDot: true }) => '.txt'
+ * @example useExtractFileExtension('file.txt') => 'txt'
  */
+export function useExtractFileExtension(input: string | { text: string, includeDot?: boolean }) {
+  let text: string
+  let includeDot = false
 
-export function useExtractFileExtension(text: string) {
+  if (typeof input === 'string') {
+    text = input
+  }
+  else {
+    text = input.text
+    includeDot = input.includeDot || false
+  }
+
   if (!text)
     return ''
 
   /* Find the last dot in the string */
   const lastDotIndex = text.lastIndexOf('.')
+
   /* If there is no dot, return the full name */
   if (lastDotIndex === -1)
     return text
-  /* Otherwise, return the extension */
-  return text.substring(lastDotIndex + 1)
+
+  /* Extract the extension */
+  const extension = text.slice(lastDotIndex + (includeDot ? 0 : 1))
+
+  return extension
 }
 
 // 'In-source Test' for the useExtractFileExtension function
@@ -24,7 +40,8 @@ if (import.meta.vitest) {
   const { it, expect } = import.meta.vitest
   it('useExtractFileExtension', () => {
     expect(useExtractFileExtension('file.txt')).toBe('txt')
-    expect(useExtractFileExtension('file')).toBe('file')
-    expect(useExtractFileExtension('')).toBe('')
+    expect(useExtractFileExtension({ text: 'file' })).toBe('file')
+    expect(useExtractFileExtension({ text: 'file.txt' })).toBe('txt')
+    expect(useExtractFileExtension({ text: 'file.txt', includeDot: true })).toBe('.txt')
   })
 }
